4d77d1074a07fa95455d240002b10b06c8c8e73e
Nate Magera <nmagera07@gmail.com>
2019-08-08 13:18:39 -0400

    adding JOTD

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'running' })
+    res.status(200).json({ 
+        jargonOfTheDay: process.env.JOTD,
+        
+     })
 })
 
 
